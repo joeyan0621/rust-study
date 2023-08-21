c15a8f7689231b1b21d7c0a6ca5eb3e52d2c562a
joe <joeyan0621@gmail.com>
2023-08-21 18:59:36 +0800

    format code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let mut arr_i32 = [45,23,78,23,3,14];
-    let mut arr_str = ["ruby", "java", "BASIC", "nest", "Lisp", "rust","Perl","Erlang"]; 
+    let mut arr_i32 = [45, 23, 78, 23, 3, 14];
+    let mut arr_str = [
+        "ruby", "java", "BASIC", "nest", "Lisp", "rust", "Perl", "Erlang",
+    ];
     bubble_sort(&mut arr_i32);
     bubble_sort_tmp(&mut arr_str);
     println!("ordered: {:?}", arr_i32);
@@ -8,23 +10,23 @@ fn main() {
 }
 
 fn bubble_sort(arr: &mut [i32]) {
-  let len = arr.len();
-  for i in 0..len {
-    for j in 0..len-i-1 {
-      if arr[j] > arr[j+1]  {
-        arr.swap(j, j + 1);
-      }
+    let len = arr.len();
+    for i in 0..len {
+        for j in 0..len - i - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
     }
-  }
 }
 
 fn bubble_sort_tmp<T: PartialOrd>(arr: &mut [T]) {
-  let len = arr.len();
-  for i in 0..len {
-    for j in 0..len-i-1 {
-      if arr[j] > arr[j+1]  {
-        arr.swap(j, j + 1);
-      }
+    let len = arr.len();
+    for i in 0..len {
+        for j in 0..len - i - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
     }
-  }
 }
